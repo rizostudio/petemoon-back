b29fed1c6f03d9e0b0d2a88d5a0464e2cc2f7209
mmz2000 <mohammadmahdizareyi@gmail.com>
2022-12-23 13:51:34 +0330

    feat(account): create app

--- config/settings/common.py
+++ config/settings/common.py
@@ -67,7 +67,7 @@ DJANGO_APPS = (
 THIRD_PARTY_APPS = ("rest_framework",)
 
 # Apps specific for this project go here.
-LOCAL_APPS = ()
+LOCAL_APPS = ("accounts",)
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # END APP CONFIGURATION
