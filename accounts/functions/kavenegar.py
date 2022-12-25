29fc26fe35198b947c442d590df6e03e5961be8c
mmz2000 <mohammadmahdizareyi@gmail.com>
2022-12-25 17:54:40 +0330

    feat(accounts): send sms functionality

---
+++ accounts/functions/kavenegar.py
@@ -0,0 +1,21 @@
+from kavenegar import APIException, HTTPException, KavenegarAPI
+
+from config.settings import KAVENEGAR_API_KEY, KAVENEGAR_TEMPLATE
+
+
+def send_sms(phone_number: str, code: str) -> bool:
+    api = KavenegarAPI(KAVENEGAR_API_KEY)
+    params = {
+        "receptor": phone_number,
+        "template": KAVENEGAR_TEMPLATE,
+        "token": code,
+        "type": "sms",
+    }
+    try:
+        api.verify_lookup(params)
+        return True
+    except APIException as e:
+        print(e)
+    except HTTPException() as e:
+        print(e)
+    return False
