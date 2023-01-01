4cb20f7007078e824e20803188f43d1aab3de974
amin <aminmalek5@gmail.com>
2023-01-01 22:35:56 +0330

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("dashboard/", include("dashboard.urls")),
 ]
