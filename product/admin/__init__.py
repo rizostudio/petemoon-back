803ea79025d3b721a2b1cf43e87412f87e3c8111
mmz2000 <mohammadmahdizareyi@gmail.com>
2023-01-02 19:16:05 +0330

    feat(product): product admin

---
+++ product/admin/__init__.py
@@ -0,0 +1,3 @@
+from product.admin.category import CategoryAdmin
+from product.admin.petshop import PetshopAdmin
+from product.admin.product import ProductAdmin
---
+++ product/admin/category.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from product.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
