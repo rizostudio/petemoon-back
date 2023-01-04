b3341e369cd450ad212627a09d0e283c04b7f9f5
mmz2000 <mohammadmahdizareyi@gmail.com>
2023-01-04 16:51:24 +0330

    feat(product): comment model

--- product/admin/product.py
+++ product/admin/product.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from product.admin.comment import CommentInline
 from product.models import Product
 
 
@@ -7,3 +8,5 @@ from product.models import Product
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "category", "animal_type", "brand")
+    inlines = [CommentInline]
