163828f7238c59c52783f7460d24ce92c8b0c373
aminmalek <aminmalek5@gmail.com>
2023-04-04 00:20:27 +0330

    add image to order

--- dashboard/serializers/order.py
+++ dashboard/serializers/order.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from shopping_cart.models import Order
+# from product.serializers.pricing import ProductPricingSerializer
+from dashboard.serializers import AddressSerializer
 
+from product.serializers.petshop import PetshopSerializer
+from product.models import ProductPricing
 
-class OrderSerializer(serializers.ModelSerializer):
-
+class ProductPricingSerializer(serializers.ModelSerializer):
+    petshop = PetshopSerializer(read_only=True)
+    picture = serializers.ImageField(source='product.picture')
     class Meta:
-        model = Order
-        fields = ['order_id','status','address','products']
-        read_only_fields = (
-            'address',
-        )
-        depth = 1
+        model = ProductPricing
+        fields = ("id", "price", "price_after_sale", "inventory", "petshop","picture")
 
+class OrderSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    order_id = serializers.CharField()
+    status = serializers.CharField()
+    address = AddressSerializer()
+    products = ProductPricingSerializer(many=True)
+    total_price = serializers.IntegerField()
+    shipping_method = "ShippingSerializer()"
