753b11097030140043e50ef7d34b615e1ac7b493
aminmalek <aminmalek5@gmail.com>
2023-04-20 23:01:33 +0330

    bug fix delete pricing

--- petshop_dashboard/views/products.py
+++ petshop_dashboard/views/products.py
@@ -7,20 +7,46 @@ from rest_framework import exceptions
 from config.responses import SuccessResponse, UnsuccessfulResponse
 from config.exceptions import CustomException
 from product.models import ProductPricing
-from ..serializers import ProductPricingSerializer
+from petshop_dashboard.serializers import PetShopProductPricingSerializer
 from product.serializers import ProductListSerializer
-from product.selectors import get_item_list
 from product.models import Petshop
+
+from config.responses import ok
+from product.serializers import ProductGetSerializer
+from product.models import Product
+
+from django.db.models import Avg, F, Max, Min, Q, Sum
+from django.db.models.functions import Coalesce
 
 
 class PetShopProductPricingView(APIView):
 
-    serializer_class = ProductPricingSerializer
+    serializer_class = PetShopProductPricingSerializer
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        products = ProductPricing.objects.filter(
-            petshop__owner__user=request.user)
+        query_params = self.request.query_params
+       
+        if query_params.get("pet_type")!= None:
+            products = ProductPricing.objects.filter(
+                petshop__owner__user=request.user,
+                product__pet_type__slug=query_params.get("pet_type"))
+        else:
+            products = ProductPricing.objects.filter(
+                petshop__owner__user=request.user)
+
+        if query_params.get("newest") == "True":
+            products = products.order_by("-created_at")
+
+
+        order_by = query_params.get("order_by")
+        
+        if order_by == "low_price":
+            products = products.order_by("price")
+        elif order_by == "high_price":
+            products = products.order_by("-price")
+
+
         result = self.serializer_class(products, many=True).data
         return SuccessResponse(data=result)
 
@@ -49,43 +75,120 @@ class PetShopProductPricingView(APIView):
         petshop = Petshop.objects.get(owner__user=request.user)
         try:
             if serialized_data.is_valid(raise_exception=True):
-                try:
-                    from product.models import Product
-                    serialized_data.validated_data["product"] = Product.objects.get(id=serialized_data.validated_data["product_id"])
-                    product_pricing = ProductPricing.objects.create(serialized_data.validated_data)
-                except CustomException :
-                    raise CustomException(detail="IUHUIOG")
+                    
+                    product_pricing = ProductPricing.objects.filter(
+                        product__id=serialized_data.validated_data["product_id"],
+                        petshop__owner__user=request.user)
+                    if product_pricing.exists():
+                        raise CustomException(detail="You created this product before")
+                    
+                    serialized_data.save(petshop=petshop)
             return SuccessResponse(data={"message":_("Product pricing added successfuly")})
         except CustomException as e:
             return UnsuccessfulResponse(errors=e.detail, status_code=e.status_code)
         except exceptions.ValidationError as e:
             return UnsuccessfulResponse(errors=e.detail, status_code=e.status_code)
+        
+    def delete(self, request, id=None):
+        try:
+            try:
+                pricing = ProductPricing.objects.get(id=id,petshop__owner__user=request.user).delete()
+            except ProductPricing.DoesNotExist:
+                raise CustomException(detail=_("Product pricing does not exist"))
+
+            return SuccessResponse(data={"message":_("product pricing deleted successfuly")})
+                
+        except CustomException as e:
+            return UnsuccessfulResponse(errors=e.detail, status_code=e.status_code) 
 
 class ProductsView(APIView):
 
     serializer_class = ProductListSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, *args, **kwargs):
         query_params = self.request.query_params
-        try:
-            kw = {
-                "limit": int(query_params.get("limit", "16")),
-                "offset": int(query_params.get("limit", "0")),
-                "pet_types": query_params.get("pet_types", "").split(","),
-                "category_slugs": query_params.get("category_slugs", "").split(
-                    ","
+        # Start with all products
+        products = Product.objects.annotate(
+            rating=Avg(
+                "comments__rate",
+                filter=Q(
+                    comments__published=True, comments__product__slug=F("slug")
                 ),
-                "brand_slugs": query_params.get("brand_slugs", "").split(","),
-                "max_price": int(query_params.get("max_price", None)),
-                "min_price": int(query_params.get("min_price", None)),
-                "order_by": query_params.get("order_by"),
-                "search": query_params.get("search", "").split("+"),
-            }
-        except CustomException:
-            kw = {}
-        try:
-            items, count = get_item_list(**kw)
-        except CustomException as e:
-            raise e
-        return SuccessResponse({"products": self.serializer_class(items, many=True).data, "count": count, })
+            )
+        ).annotate(
+            min_price=Min(
+                Coalesce(
+                    "productpricing__price_after_sale", "productpricing__price"
+                ),
+                filter=Q(
+                    productpricing__inventory__gt=0,
+                    productpricing__product=F("id"),
+                ),
+            )
+        ).annotate(
+            max_price=Max(
+                "productpricing__price",
+                filter=Q(
+                    productpricing__inventory__gt=0,
+                    productpricing__product=F("id"),
+                ),
+            )
+        ).annotate(
+            inventory=Sum(
+                "productpricing__inventory",
+                filter=Q(productpricing__product=F("id")),
+            )
+        )
+        
+        pet_types = query_params.get("pet_types", "").split(",")
+        if pet_types != ['']:
+            products = products.filter(pet_type__slug__in=pet_types)
+
+        # Filter by pet categories
+        pet_categories = query_params.get("pet_categories", "").split(",")
+        if pet_categories != ['']:
+            products = products.filter(category__slug__in=pet_categories)
+
+
+        # # Filter by brand slugs
+        brand_slugs = query_params.get("brand_slugs", "").split(",")
+        if brand_slugs != ['']:
+            products = products.filter(brand__slug__in=brand_slugs)
+
+        # Filter by price range
+        max_price = query_params.get("max_price",None)
+        min_price = int(query_params.get("min_price","0"))
+        
+        if max_price is not None:
+            products = products.filter(max_price__lte=int(max_price))
+        if min_price is not None:
+            products = products.filter(min_price__gte=min_price)
+
+        # Order the results
+        order_by = query_params.get("order_by")
+        if order_by == "name":
+            products = products.order_by("name")
+        elif order_by == "min_price":
+            products = products.order_by("min_price")
+        elif order_by == "max_price":
+            products = products.order_by("max_price")
+        elif order_by == "popular":
+            products = products.order_by("rate")
+        elif order_by == "newest":
+            products = products.order_by("productpricing__created_at")
+
+
+        # Apply limit and offset
+        limit = int(query_params.get("limit", "16"))
+        offset = int(query_params.get("offset", "0"))
+        products = products[offset:offset+limit]
+
+        # If no products are found, raise a 404 exception
+  
+        return ok(
+            {"products": ProductGetSerializer(products, many=True).data,
+             "count": products.count()}
+             )
+
+
