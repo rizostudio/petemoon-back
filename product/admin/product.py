de68d9637f7e8d172ecc71e843b1f9a3797e151f
Farzam Rezaei <87132784+farzamrezaei@users.noreply.github.com>
2023-06-20 14:25:13 +0330

    Update product/admin/product.py

--- product/admin/product.py
+++ product/admin/product.py
@@ -6,10 +6,10 @@ from product.admin.spec import SpecInline
 from product.models import Product
 
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     search_fields = ["name"]
-#     prepopulated_fields = {"slug": ("name",)}
-#     list_display = ("name", "category", "pet_type", "brand")
-#     inlines = [CommentInline, PictureInline, SpecInline]
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "category", "pet_type", "brand")
+    inlines = [CommentInline, PictureInline, SpecInline]
+#admin.site.register(Product)
