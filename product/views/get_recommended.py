ea1a9cfc90a147b0e63fca1cbe8bd7fdb54474f6
Fan Wang <nimadorostkar97@gmail.com>
2023-07-17 13:06:07 +0330

    change APIs products serializers

--- product/views/get_recommended.py
+++ product/views/get_recommended.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 
 from config.responses import ok
 from product.selectors import get_recommended_products
-from product.serializers import ProductListSerializer
-
+from product.serializers import ProductListSerializer, ProductGetSerializer
 
 class GetRecommended(APIView):
     def get(self, *args, **kwargs):
         user = self.request.user
         products = get_recommended_products(user)
-        return ok(data=ProductListSerializer(products, many=True).data)
+        return ok(data=ProductGetSerializer(products, many=True).data)
+
+
+
--- product/views/get_sales.py
+++ product/views/get_sales.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 
 from config.responses import ok
 from product.selectors import get_on_sales
-from product.serializers import ProductListSerializer
+from product.serializers import ProductListSerializer, ProductGetSerializer
 
 
 class GetSales(APIView):
@@ -10,5 +10,5 @@ class GetSales(APIView):
         limit = self.request.query_params.get("limit", 16)
         offset = self.request.query_params.get("offset", 0)
         products = get_on_sales(limit=limit, offset=offset)
-        serializer = ProductListSerializer(products, many=True)
+        serializer = ProductGetSerializer(products, many=True)
         return ok(data=serializer.data)
