55ea561e961ccbef3f84b22a998396dec36779cc
Fan Wang <nimadorostkar97@gmail.com>
2023-08-06 21:30:44 +0330

    UserProfileSerializer update method debug

--- dashboard/serializers/user_profile.py
+++ dashboard/serializers/user_profile.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 
 
 class UserProfileSerializer(serializers.Serializer):
-    first_name = serializers.CharField(read_only=True)
-    last_name = serializers.CharField(read_only=True)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
     phone_number = serializers.CharField(read_only=True)
     email = serializers.EmailField()
     birth_date = serializers.DateField(source="profile.birth_date")
@@ -14,6 +14,10 @@ class UserProfileSerializer(serializers.Serializer):
             user.email = validated_data['email']
         if validated_data['profile']['birth_date']:
             user.profile.birth_date = validated_data['profile']['birth_date']
+        if validated_data['first_name']:
+            user.first_name = validated_data['first_name']
+        if validated_data['last_name']:
+            user.last_name = validated_data['last_name']
         user.save()
         user.profile.save()
         return user
