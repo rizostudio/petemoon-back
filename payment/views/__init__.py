5789c757baa84b447bceeb3263111ecd4f67ec6b
Fan Wang <nimadorostkar97@gmail.com>
2023-08-08 10:36:40 +0330

    DiscountSerializer and generate API

--- payment/views/__init__.py
+++ payment/views/__init__.py
@@ -1,2 +1,3 @@
 from payment.views.get_transaction import GetTransactionList
 from payment.views.verify_transaction import VerifyTransaction, SendReqTransaction
+from payment.views.generate_discount import GenerateDiscount
---
+++ payment/views/generate_discount.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from payment.serializers import DiscountSerializer
+from accounts.views.permissions import IsPetShop
+from config.responses import SuccessResponse, UnsuccessfulResponse
+from rest_framework import status
+
+
+class GenerateDiscount(APIView):
+    permission_classes = [IsPetShop]
+
+    def post(self, request):
+        data = request.data
+        data['creator'] = request.user.id
+        serializer = DiscountSerializer(data=data)
+        if serializer.is_valid():
+            serializer.validated_data
+            serializer.save()
+            return SuccessResponse(data={'status': True, 'data': serializer.data })
+        else:
+            return UnsuccessfulResponse(errors=serializer.errors, status_code=status.HTTP_406_NOT_ACCEPTABLE)
