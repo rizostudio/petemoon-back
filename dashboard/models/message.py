4849244ab4344e9b5d03314076aca5c2be821444
Fan Wang <nimadorostkar97@gmail.com>
2023-08-10 19:32:18 +0330

    DiscountCalculator

--- dashboard/models/message.py
+++ dashboard/models/message.py
@@ -3,12 +3,11 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-
 class Message(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     context = models.TextField()
     title = models.CharField(max_length=256,null=True)
-    #created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.title) +' | '+ str(self.user)
--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from payment.views import GetTransactionList, VerifyTransaction, SendReqTransaction, GenerateDiscount
+from payment.views import GetTransactionList, VerifyTransaction, SendReqTransaction, GenerateDiscount, DiscountCalculator
 
 urlpatterns = [
     path("generate-discount/",GenerateDiscount.as_view(),name="generate-discount"),
+    path("discount-calculator/",DiscountCalculator.as_view(),name="discount-calculator"),
     path("", GetTransactionList.as_view(), name="get-transaction-list"),
     path("send-req/<int:transaction_id>/",SendReqTransaction.as_view(),name="send-req-transaction"),
     path("verify/<int:transaction_id>/",VerifyTransaction.as_view(),name="verify-transaction"),
