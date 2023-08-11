031588bb9c8ea4b3d1745830c9587f9f5c146ded
Fan Wang <nimadorostkar97@gmail.com>
2023-08-11 23:13:59 +0330

    debug

--- vet/models/reserve_times.py
+++ vet/models/reserve_times.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class ReserveTimes(models.Model):
-    vet = models.ForeignKey("accounts.VetProfile", on_delete=models.CASCADE, related_name="vet_reserve")
+    vet = models.ForeignKey("accounts.VetProfile", on_delete=models.CASCADE, related_name="vet_reserve",blank=True, null=True)
     time = models.DateTimeField()
     availabe = models.BooleanField(default=True)
     reserved = models.BooleanField(default=False)
