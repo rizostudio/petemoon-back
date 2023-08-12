81e37e09f22921818c71af7a80e1026c8b488a66
nima <54316106+nimadorostkar@users.noreply.github.com>
2023-08-12 04:10:48 +0330

    Update register_petshop.py

--- accounts/views/register_petshop.py
+++ accounts/views/register_petshop.py
@@ -49,6 +49,10 @@ class RegisterPetshop(APIView):
                 user.first_name = serialized_data.validated_data['first_name']
                 user.last_name = serialized_data.validated_data['last_name']
                 user.save()
+                petshop_profile = PetshopProfile.objects.get(user=user)
+                petshop_profile.first_name = serialized_data.validated_data['first_name']
+                petshop_profile.last_name = serialized_data.validated_data['last_name']
+                petshop_profile.save()
                 return SuccessResponse(data=self.serializer_class(petshop,many=True).data)
 
         except CustomException as e:
