505bef21e303adf99d1c89d082b22b4e0e1f5931
Fan Wang <nimadorostkar97@gmail.com>
2023-08-13 22:37:13 +0330

    petshop_name debug

--- accounts/views/register_petshop.py
+++ accounts/views/register_petshop.py
@@ -53,9 +53,12 @@ class RegisterPetshop(APIView):
                 petshop_profile.first_name = serialized_data.validated_data['first_name']
                 petshop_profile.last_name = serialized_data.validated_data['last_name']
                 petshop_profile.save()
-                petshop_petshop = Petshop.objects.get(owner=petshop_profile)
-                petshop_petshop.name = request.data['petshop_name']
-                petshop_petshop.save()
+                try:
+                    petshop_petshop = Petshop.objects.get(owner=petshop_profile)
+                    petshop_petshop.name = request.data['petshop_name']
+                    petshop_petshop.save()
+                except:
+                    pass
                 return SuccessResponse(data=self.serializer_class(petshop,many=True).data)
 
         except CustomException as e:
