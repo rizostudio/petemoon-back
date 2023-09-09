ab057e2a0e79c4341d61f67f82c2cea74c15eef4
Fan Wang <nimadorostkar97@gmail.com>
2023-09-09 23:11:23 +0330

    push11

--- accounts/views/register_vet.py
+++ accounts/views/register_vet.py
@@ -66,7 +66,13 @@ class VetRegisterView(APIView):
                     user=request.user),validated_data=serialized_data.validated_data)
                 user = request.user
                 user.register_completed = True
+                user.first_name = request.data['first_name']
+                user.last_name = request.data['last_name']
                 user.save()
+                vet = VetProfile.objects.get(user=request.user)
+                vet.first_name = request.data['first_name']
+                vet.last_name = request.data['last_name']
+                vet.save()
                 return SuccessResponse(data=self.serializer_class(vet_profile,many=True).data)
 
         except CustomException as e:
